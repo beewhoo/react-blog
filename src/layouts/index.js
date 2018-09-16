9d0304931baed42542b5d5a2792d40cf275bf3f6
bibek <bibeksinghthapa@gmail.com>
2018-09-16 12:22:00 -0400

    'blog up and running - import react media so the website is responsive'

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,47 +1,128 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import Header from '../components/header'
-import './index.css'
+import "./index.css";
 import "../styles/layout-overide.css";
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
+import Media from "react-media";
 
+const Header = () => (
+  <div
+    style={{
+      background: "#f5f5f5",
+      marginBottom: "3rem",
+      borderBottom: "2px solid #e6e6e6"
+    }}
+  >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        margin: "0 auto",
+        maxWidth: 980,
+        padding: "1.45rem 1.0875rem"
       }}
     >
-      {children()}
+      <h1 style={{ margin: 0, textAlign: "center", fontSize: "18px" }}>
+        <Link
+          to="/"
+          style={{
+            color: "black",
+            textDecoration: "none"
+          }}
+        >
+          Blog
+        </Link>
+      </h1>
     </div>
   </div>
-)
+);
 
-Layout.propTypes = {
-  children: PropTypes.func,
-}
+const Sidebar = props => (
+  <div
+    style={{
+      border: "2px solid #e6e6e6",
+      maxWidth: 960,
+      padding: "0.5rem",
+      marginBottom: "25px"
+    }}
+  >
+    <strong>{props.title}.</strong> {props.description}
+  </div>
+);
 
-export default Layout
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"
+      }}
+    >
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                {children()}
+              </div>
 
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+              <div style={{ flex: 1 }}>
+                <Sidebar
+                  title="Blog"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                />
+                <Sidebar
+                  title="About author"
+                  description="Lorem ipsum dolor sit amet, consectetur aco laboris, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
+    </div>
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+};
+
+export default TemplateWrapper;
